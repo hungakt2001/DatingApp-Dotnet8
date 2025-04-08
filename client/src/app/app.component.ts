d009e670c8b36fd4008ed61a12be88637726ec2b
hungtn <hungtn@ftech.ai>
2025-04-08 22:35:36 +0700

    50-section5

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,20 +2,34 @@ import { CommonModule, NgFor } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavComponent } from './nav/nav.component';
+import { AccountService } from './_service/account.service';
+import { HomeComponent } from "./home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [CommonModule, NavComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent implements OnInit {
+  private accountService = inject(AccountService);
   http = inject(HttpClient);
   title = 'Dating App';
   users: any;
 
   ngOnInit(): void {
+    this.getUser();
+    this.setCurretUser();
+  }
+  setCurretUser() {
+    const userString = localStorage.getItem('user');
+    if (!userString) return;
+    const user = JSON.parse(userString);
+    this.accountService.currentUser.set(user);
+  }
+  getUser() {
     this.http.get('https://localhost:5001/api/user').subscribe({
       next: (response) => (this.users = response),
       error: (error) => console.log(error),
