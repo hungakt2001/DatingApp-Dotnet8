5e8d4db45048526a26c3b62197cb035e547a3a2c
hungtn <hungtn@ftech.ai>
2025-04-11 22:04:37 +0700

    End Section 6

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,23 +1,22 @@
 import { CommonModule, NgFor } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './nav/nav.component';
 import { AccountService } from './_service/account.service';
-import { HomeComponent } from "./home/home.component";
+import { HomeComponent } from './home/home.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, NavComponent, HomeComponent],
+  imports: [CommonModule, NavComponent, HomeComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent implements OnInit {
   private accountService = inject(AccountService);
-  
+
   title = 'Dating App';
-  
 
   ngOnInit(): void {
     this.setCurretUser();
@@ -28,5 +27,4 @@ export class AppComponent implements OnInit {
     const user = JSON.parse(userString);
     this.accountService.currentUser.set(user);
   }
-  
 }
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,27 +1,37 @@
+import { Message } from './../../../node_modules/esbuild/lib/main.d';
 import { FormsModule } from '@angular/forms';
 import { Component, inject } from '@angular/core';
 import { AccountService } from '../_service/account.service';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { TitleCasePipe } from '@angular/common';
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [FormsModule, BsDropdownModule],
+  imports: [FormsModule, BsDropdownModule, RouterLink, RouterLinkActive, TitleCasePipe],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css',
 })
 export class NavComponent {
   accountService = inject(AccountService);
+  private router = inject(Router);
+  private toastr = inject(ToastrService);
   model: any = {};
 
   login() {
     this.accountService.login(this.model).subscribe({
-      next: (response) => {
-        console.log(response);
+      next: (_) => {
+        this.router.navigateByUrl('/members');
       },
-      error: (error) => console.log(error),
+      error: (error) => {
+        console.log(this.model); // 👈 log toàn bộ object
+        this.toastr.error(error.error); // hiện tại gây lỗi
+      },
     });
   }
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }
